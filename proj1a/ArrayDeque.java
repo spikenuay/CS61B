bc6c51c2dd7c194f573535b6462e96449d51509f
spike_nauy <yuanyongwei42@gmail.com>
2024-03-10 14:52:21 +0800

    project1a 10rd

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -3,27 +3,24 @@ public class ArrayDeque<T> {
     private int left;
     private int right;
     private int capacity = 8;
-    public ArrayDeque(){
+    public ArrayDeque() {
         items = (T[]) new Object[capacity];
         left = right = 0;
     }
-    private void resize(int newsize){
+    private void resize(int newsize) {
         T[] newAlist = (T[]) new Object[newsize];
         int size = size();
         //复制元素到新数组 正常数组顺序
-        if(left < right){
-            for(int i = left,j = 0; i < right && j < size; i++, j++){
+        if (left < right) {
+            for (int i = left, j = 0; i < right && j < size; i++, j++) {
                 newAlist[j] = items[i];
             }
-        }
-        //复制元素到新数组 考虑环绕的情况 分两次
-        // 首先复制从 left 到数组末尾的元素，然后复制从数组开头到 right 的元素
-        else if(left > right){
+        } else if (left > right) {
             int j = 0;
-            for(int i = left; j < capacity - left; i++, j++){
+            for (int i = left; j < capacity - left; i++, j++) {
                 newAlist[j] = items[i];
             }
-            for(int i = 0; j < size; i ++, j++){
+            for (int i = 0; j < size; i++, j++) {
                 newAlist[j] = items[0];
             }
         }
@@ -31,86 +28,85 @@ public class ArrayDeque<T> {
         right = size;
         items = newAlist;
         capacity = newsize;
-    }
-    private boolean isFull(){
+    } //复制元素到新数组 考虑环绕的情况 分两次
+    // 首先复制从 left 到数组末尾的元素，然后复制从数组开头到 right 的元素
+    private boolean isFull() {
         return size() == capacity - 1;
     }
-    public void addFirst(T item){
-        if(isFull()){
+    public void addFirst(T item) {
+        if (isFull()) {
             resize((int) (capacity * 1.5));
         }
         left = (left - 1 + capacity) % capacity;
         items[left] = item;
     }
-    public  void addLast(T item){
-        if (isFull()){
+    public  void addLast(T item) {
+        if (isFull()) {
             resize((int) (capacity * 1.5));
         }
         items[right] = item;
         right = right + 1;
     }
-    public  boolean isEmpty(){
+    public  boolean isEmpty() {
         return  left == right;
     }
-    public int size(){
+    public int size() {
         return (right - left + capacity) % capacity;
     }
-    public  void printDeque(){
-        if (left < right){
-            for (int i = 0; i <= right - 1; i++){
+    public  void printDeque() {
+        if (left < right) {
+            for (int i = 0; i <= right - 1; i++) {
                 System.out.print(items[i] + " ");
             }
         }
-        else if(left > right){
-            for (int i = left; i <= capacity - 1; i++){
+        else if (left > right) {
+            for (int i = left; i <= capacity - 1; i++) {
                 System.out.print(items[i] + " ");
             }
-            for (int i = 0; i <= right - 1; i++){
+            for (int i = 0; i <= right - 1; i++) {
                 System.out.print(items[i] + " ");
             }
         }
     }
-    public T removeFirst(){
-        if (isEmpty()){
+    public T removeFirst() {
+        if (isEmpty()) {
             return  null;
         }
         T res = items[left];
         left = (left + 1) % capacity;
-        if (isLowUsageRate()){
+        if (isLowUsageRate()) {
             resize((int) (capacity * 0.5));
         }
         return res;
     }
-    public T removeLast(){
-        if (isEmpty()){
+    public T removeLast() {
+        if (isEmpty()) {
             return null;
         }
         T res = items[right - 1];
         right = right - 1;
-        if (isLowUsageRate()){
+        if (isLowUsageRate()) {
             resize((int) (capacity * 0.5));
         }
         return res;
     }
-    public T get(int index){
-        if (index < 0 || index >= size() || isEmpty()){
+    public T get(int index) {
+        if (index < 0 || index >= size() || isEmpty()) {
             return null;
         }
-        if (left < right){
+        if (left < right) {
             return items[index];
-        }
-        else if(left >right){
-            if (index + left < capacity){
+        } else if (left > right) {
+            if (index + left < capacity) {
                 return items[index + left];
-            }
-            else {
+            } else {
                 return items[index - left];
             }
         }
         return null;
     }
-    private boolean isLowUsageRate(){
-        if (capacity >= 16 && size() / capacity < 0.25){
+    private boolean isLowUsageRate() {
+        if (capacity >= 16 && size() / capacity < 0.25) {
             return true;
         }
         return false;
--- proj1a/LinkedListDeque.java
+++ proj1a/LinkedListDeque.java
@@ -13,46 +13,46 @@ public class LinkedListDeque<T> {
     }
     private LLDNode sentinel;
     private int size;
-    public LinkedListDeque(){
+    public LinkedListDeque() {
         sentinel = new LLDNode(null, null, null);
         sentinel.next = sentinel;
         sentinel.prev = sentinel;
         size = 0;
     }
-    public void addFirst(T item){
-        LLDNode Node = new LLDNode(sentinel , sentinel.next, item);
+    public void addFirst(T item) {
+        LLDNode Node = new LLDNode(sentinel, sentinel.next, item);
         sentinel.next.prev = Node;
         sentinel.next = Node;
         size += 1;
     }
-    public void addLast(T item){
+    public void addLast(T item) {
         LLDNode Node = new LLDNode(sentinel.prev, sentinel, item);
         sentinel.prev.next = Node;
         sentinel.prev = Node;
         size += 1;
     }
 
-    public boolean isEmpty(){
-        if(size == 0)
+    public boolean isEmpty() {
+        if (size == 0) {
             return true;
+        }
         return false;
     }
-    public int size(){
+    public int size() {
         return size;
     }
-    public void printDeque(){
-        if(size == 0){
+    public void printDeque() {
+        if (size == 0) {
             return;
-        }
-        else {
-            for(int i = 0; i <= size - 1; i += 1){
+        } else {
+            for (int i = 0; i <= size - 1; i += 1) {
                 System.out.print(get(i) + " ");
             }
         }
 
     }
-    public T removeFirst(){
-        if(size == 0){
+    public T removeFirst() {
+        if (size == 0) {
             return null;
         }
 
@@ -62,8 +62,8 @@ public class LinkedListDeque<T> {
         size -= 1;
         return item;
     }
-    public T removeLast(){
-        if(size == 0){
+    public T removeLast() {
+        if (size == 0) {
             return null;
         }
         T item = sentinel.prev.item;
@@ -72,31 +72,32 @@ public class LinkedListDeque<T> {
         size -= 1;
         return  item;
     }
-    public T get(int index){
-        if (index < 0 || index >= size)
+    public T get(int index) {
+        if (index < 0 || index >= size) {
             return  null;
-        else {
+        } else {
             LLDNode temp = sentinel;
-            for(int i = 0; i <= index; i += 1){
+            for (int i = 0; i <= index; i += 1) {
                 temp = temp.next;
             }
             return temp.item;
         }
 
     }
-    public T getRecursive(int index){
-        if(index < 0 || index >= size || sentinel.next == null)
+    public T getRecursive(int index) {
+        if (index < 0 || index >= size || sentinel.next == null) {
             return null;
-        else if(index == 0)
+        } else if (index == 0) {
             return sentinel.next.item;
-        else
+        } else {
             return getRecursiveHelper(sentinel.next, index);
+        }
     }
-    private T getRecursiveHelper(LLDNode Node, int index){
-        if(index == 0)
+    private T getRecursiveHelper(LLDNode Node, int index) {
+        if (index == 0) {
             return Node.item;
-        else
+        } else {
             return getRecursiveHelper(Node.next, index - 1);
+        }
     }
-
 }
